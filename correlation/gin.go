c657743f93a5dd06a9676e775756428429c2493d
nitinjangam <njangam169@gmail.com>
2023-08-18 10:57:04 +0200

    debug

--- correlation/gin.go
+++ correlation/gin.go
@@ -2,6 +2,7 @@ package correlation
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -12,10 +13,11 @@ import (
 func TraceMiddleware(c *gin.Context) {
 	// Check if trace ID exists in the request headers
 	traceID := c.GetHeader("X-Trace-ID")
-
+	fmt.Println("got traceID from the context:", traceID)
 	// If trace ID doesn't exist, generate a new one
 	if traceID == "" {
 		traceID = uuid.New().String()
+		fmt.Println("empty traceID from the context so creating new:", traceID)
 	}
 
 	// Create a new logger with the traceID ingested as correlationID
@@ -27,6 +29,8 @@ func TraceMiddleware(c *gin.Context) {
 	// Add context with logger instance back to request context
 	c.Request = c.Request.WithContext(ctx)
 
+	log := logger.FromContext(c.Request.Context())
+	log.Debug("logging with logger created after storing in the context")
 	// Continue processing the request
 	c.Next()
 }
