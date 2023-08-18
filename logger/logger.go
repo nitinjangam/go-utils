0abdb2c5ae301999d65aa6a7360273f4d88090b1
nitinjangam <njangam169@gmail.com>
2023-08-18 11:36:51 +0200

    debug

--- logger/logger.go
+++ logger/logger.go
@@ -2,7 +2,6 @@ package logger
 
 import (
 	"context"
-	"fmt"
 
 	"golang.org/x/exp/slog"
 )
@@ -23,13 +22,11 @@ func Init(logger *slog.Logger) {
 
 // New adds correlationID to logger fields and embed it in given context.
 func New(ctx context.Context, correlationID string) context.Context {
-	fmt.Println("creating new logger with correlation id:", correlationID)
 	return ToContext(ctx, Logger.With(slog.String(IDJsonKey, correlationID)))
 }
 
 // ToContext adds given logger to given context
 func ToContext(ctx context.Context, logger *slog.Logger) context.Context {
-	fmt.Println("adding newly created logger with loggerkey:", loggerKey)
 	return context.WithValue(ctx, loggerKey, logger)
 }
 
@@ -37,16 +34,12 @@ func ToContext(ctx context.Context, logger *slog.Logger) context.Context {
 // logger, it uses application default logger initialized in config
 func FromContext(ctx context.Context) *slog.Logger {
 	if ctx == nil {
-		fmt.Println("returning default logger as context is nil")
 		return Logger
 	}
 
-	fmt.Println("value from context", ctx.Value(loggerKey))
 	if logger, ok := ctx.Value(loggerKey).(*slog.Logger); ok {
-		fmt.Println("got logger from context with loggerkey passed")
 		return logger
 	}
 
-	fmt.Println("couldn't find logger from context with loggerkey passed")
 	return Logger
 }
